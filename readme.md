1c5d21a56dca06db9d4baad83a33c80b6e36e47b
ag <akshith.573@gmail.com>
2024-03-19 11:18:39 -0700

    codeql linux setup

--- readme.md
+++ readme.md
@@ -1,1 +1,20 @@
 # Linux + CodeQL Experiments
+
+## Generate CodeQL Database
+
+```sh
+./setup-codeql-db.sh
+```
+
+## Additional Resources
+
+```sh
+codeql pack download trailofbits/cpp-queries trailofbits/go-queries
+```
+
+## Run CodeQL Analysis
+
+```sh
+codeql database analyze linux-codeql.db --format=sarif-latest --output=results.sarif -- codeql/cpp-queries
+codeql database analyze linux-codeql.db --format=sarif-latest --output=./tob.sarif -- trailofbits/cpp-queries
+```
--- setup-codeql-db.sh
+++ setup-codeql-db.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# env
+# LINUXSRC: path to the linux kernel source
+# codeql is installed
+
 # assert codeql is installed
 command -v codeql >/dev/null 2>&1 || {
     echo >&2 "CodeQL is not installed.  Aborting."
@@ -13,9 +17,43 @@ if [ -z ${LINUXSRC+x} ]; then
     exit 1
 fi
 
+# input
+# --kernel-version / -v: kernel version to build
+# --config / -c: kernel config to use
+
+# usage
+function usage {
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "  -v, --kernel-version <version>  Kernel version to build (default: research-v5.10)"
+    echo "  -c, --config <config>           Kernel config to use (default: defconfig)"
+    exit 1
+}
+
+# defaults
 VERSION="research-v5.10"
 CONFIG="defconfig"
-BUILD_DIR="$PWD/linux"
+
+# parse input
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+    -v | --kernel-version)
+        VERSION="$2"
+        shift
+        shift
+        ;;
+    -c | --config)
+        CONFIG="$2"
+        shift
+        shift
+        ;;
+    *)
+        usage
+        ;;
+    esac
+done
+BUILD_DIR="$PWD/linux-$VERSION"
 
 function prepare_kernel {
     echo "Preparing kernel"
@@ -26,7 +64,8 @@ function prepare_kernel {
 
 function build_with_codeql {
     echo "Building kernel with CodeQL"
-    codeql database create linux-codeql.db --language=cpp --source-root=$LINUXSRC --command="make -C $LINUXSRC O=$BUILD_DIR -j$(nproc)"
+    CODEQL_DB="linux-$VERSION-codeql.db"
+    codeql database create $CODEQL_DB --language=cpp --source-root=$LINUXSRC --command="make -C $LINUXSRC O=$BUILD_DIR -j$(nproc)"
 }
 
 prepare_kernel
